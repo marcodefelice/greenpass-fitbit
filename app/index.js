4d235848e4cf099653d4923e36a7d0fd2bbd9e4e
Marco De Felice <marco.defelice890@gmail.com>
2022-03-04 10:16:36 +0100

    Update index.js
    simple upload image script

--- app/index.js
+++ app/index.js
@@ -1,20 +1,50 @@
-import { inbox } from "file-transfer"
+import { me } from "appbit";
+import { display } from "display";
+import document from "document";
 import * as fs from "fs";
+import { inbox } from "file-transfer";
 
-function processAllFiles() {
-  let myImage = document.getElementById("greenpass")
-  inbox.onnewfile = () => {
+const SETTINGS_FILE = "settings.cbor";
+const SETTINGS_TYPE = "cbor";
+
+
+let mySettings;
+loadSettings();
+me.onunload = saveSettings;
+
+
+inbox.onnewfile = () => {
   let fileName;
   do {
     fileName = inbox.nextFile();
-    console.log(fileName)
     if (fileName) {
-      myImage.href = `/private/data/${fileName}`
+      if (mySettings.bg && mySettings.bg !== "") {
+        fs.unlinkSync(mySettings.bg);
+      }
+      mySettings.bg = `/private/data/${fileName}`;
+      applySettings();
     }
   } while (fileName);
 };
- 
 
+function loadSettings() {
+  try {
+    mySettings = fs.readFileSync(SETTINGS_FILE, SETTINGS_TYPE);
+  } catch (ex) {
+    console.error(ex)
+    mySettings = {};
+  }
+  applySettings();
 }
 
-processAllFiles();
+function saveSettings() {
+  fs.writeFileSync(SETTINGS_FILE, mySettings, SETTINGS_TYPE);
+}
+
+function applySettings() {
+  var imageBackground = document.getElementById("greenpass")
+  if (mySettings.bg) {
+    imageBackground.image = mySettings.bg;
+  }
+  display.on = true;
+}
